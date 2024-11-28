882a817ff89d9afff96e60f6830ac5c47583d141
zhizhifeng <chaoyurun@gmail.com>
2024-11-28 09:11:30 +0000

    Finish Task 2

--- SiLagoNN/syn/scr/dc_flat.tcl
+++ SiLagoNN/syn/scr/dc_flat.tcl
@@ -30,7 +30,9 @@
 ################################################################################
 
 #1. source setup file to extract global libraries
+source ../syn/synopsys_dc.setup
 #2. set the TOP_NAME of the design
+set TOP_NAME drra_wrapper
 
 # Directories for output material
 set REPORT_DIR  ../syn/rpt;      # synthesis reports: timing, area, etc.
@@ -39,19 +41,33 @@ set SOURCE_DIR ../rtl;           # rtl code that should be synthesised
 set SYN_DIR ../syn;              # synthesis directory, synthesis scripts constraints etc.
 
 #3. set hierarchy file to read the design 
-
+set hierarchy_files [split [read [open ${SOURCE_DIR}/${TOP_NAME}_hierarchy.txt r]] "\n"]
 #4. analyze design
-
+foreach filename [lrange ${hierarchy_files} 0 end-1] {
+    puts "${filename}"
+    analyze -format VHDL -lib WORK "${SOURCE_DIR}/${filename}"
+}
 #5. elaborate the design, link, and uniquify
-
+elaborate ${TOP_NAME}
+link
+uniquify
 #6. set current_design 
-
+current_design ${TOP_NAME}
 #7. set wireload model
-
+set_wire_load_mode segmented
+set_wire_load_model -name TSMC8K_Lowk_Aggresive
+set_operating_condition NCCOM
 #8. source sdc file
-
+source ${SYN_DIR}/constraints.sdc
 #9. compile
-
+compile -map_effort medium
 #10. report area, timing, power, constraints, cell in the report directory with a suitable name
-
+report_area > ${REPORT_DIR}/${TOP_NAME}_area.txt
+report_timing > ${REPORT_DIR}/${TOP_NAME}_timing.txt
+report_power > ${REPORT_DIR}/${TOP_NAME}_power.txt
+report_constraints > ${REPORT_DIR}/${TOP_NAME}_constraints.txt
+report_cell > ${REPORT_DIR}/${TOP_NAME}_cell.txt
 #11. export the netlist, ddc and sdf file in out direcory with a suitable name
+write_file -hierarchy -format ddc -output ${OUT_DIR}/${TOP_NAME}.ddc
+write_file -hierarchy -format verilog -output ${OUT_DIR}/${TOP_NAME}.v
+write_sdf ${OUT_DIR}/${TOP_NAME}.sdf
